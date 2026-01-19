b8206c974980693e2eae42b5299b1b8cac9ed134
mike4403 <mike4404@naver.com>
2026-01-19 14:13:38 +0900

    ver 1.0.4 / layout.tsx fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
 // app/_layout.tsx
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-// import "../global.css";  <-- 이 줄이 범인입니다. 지워야 합니다!
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
