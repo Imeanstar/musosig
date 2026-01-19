c19f15c6b1804f02c9257c707f52bb4616424bbf
mike4403 <mike4404@naver.com>
2026-01-19 13:58:24 +0900

    ver 1.0.3 / error patching

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,9 @@ module.exports = function (api) {
     api.cache(true);
     return {
       presets: ['babel-preset-expo'],
+      plugins: [
+        // 👇 이 줄이 반드시 있어야 하고, 꼭 "맨 마지막"에 있어야 합니다!
+        'react-native-reanimated/plugin', 
+      ],
     };
   };
